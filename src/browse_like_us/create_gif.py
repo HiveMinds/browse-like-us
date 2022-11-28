31f1e43cb5bd0238a102ef25154c58dd2b980653
a-t-0 <you@example.com>
2022-11-28 22:51:11 +0100

    Included __main__.py

--- src/browse_like_us/create_gif.py
+++ src/browse_like_us/create_gif.py
@@ -1,10 +1,14 @@
 """Merges a few images into a single gif."""
 
+from typing import List
+
 import imageio
 
 
 # Create gif file for frontpage meme.
-def create_gif(filenames: str, output_path: str, interval_sec: float) -> None:
+def create_gif(
+    filenames: List[str], output_path: str, interval_sec: float
+) -> None:
     """Merges a few images into a single gif."""
 
     images = []
